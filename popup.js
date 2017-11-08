8f46175eca3a098c02be30f2b1b1b524179f56bc
Andy <amckay@mozilla.com>
2017-11-08 11:01:50 -0800

    add in versions

--- popup.js
+++ popup.js
@@ -1,19 +1,20 @@
 var URLs = {
  'manage': 'https://addons.mozilla.org/LOCALE/developers/addon/ADDON-NAME/edit',
- 'stats': 'https://addons.mozilla.org/LOCALE/firefox/addon/ADDON-NAME/statistics/'
+ 'versions': 'https://addons.mozilla.org/LOCALE/firefox/developers/addon/ADDON-NAME/versions',
+ 'stats': 'https://addons.mozilla.org/LOCALE/firefox/addon/ADDON-NAME/statistics'
 }
 
 browser.tabs.query({currentWindow: true, active: true})
 .then((tabs) => {
   var tab = tabs[0];
-  var url = new URL(tab.url); 
+  var url = new URL(tab.url);
   var parts = url.pathname.split('\/');
-    
+
 
   if (parts[3] === "addon" && parts[4]) {
     let addonName = parts[4];
     let locale = parts[1];
-    
+
     for (let key of Object.keys(URLs)) {
       let value = URLs[key].replace('LOCALE', locale).replace('ADDON-NAME', addonName);
       document.getElementById(key).href = value;
